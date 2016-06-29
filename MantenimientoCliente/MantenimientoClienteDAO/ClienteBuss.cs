30426fc857249a72f72ddc151df99fb4d9737bff
victor1188 <victor11081995@hotmail.com>
2016-06-29 01:14:42 -0500

    Pruebas Unitarias

---
+++ MantenimientoCliente/MantenimientoClienteDAO/ClienteBuss.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MantenimientoCliente.Models;
+
+
+namespace MantenimientoClienteDAO
+{
+    public class ClienteBuss: Generico<Cliente>
+    {
+        public override Cliente Obtener(int id)
+        {
+            Cliente resultado = null;
+            using (var context = new MantenimientoClienteEntities())
+            {
+                try
+                {
+                    resultado = context.Cliente.Where(x => x.ClienteId == id).Select(x => x).FirstOrDefault();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            return resultado;
+        }
+        public new List<Cliente> Listar()
+        {
+            //inicio
+            List<Cliente> entidades = null;
+            using (var context = new MantenimientoClienteEntities())
+            {
+                try
+                {
+                    entidades = context.Set<Cliente>().Select(x => x).ToList();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            return entidades;
+            //fin
+        }
+
+        public override Cliente Buscar(Cliente entidad)
+        {//inicio
+            Cliente clienteTemp;
+
+            using (var context = new MantenimientoClienteEntities())
+            {
+                try
+                {
+                    clienteTemp = context.Cliente.Where(x => x.Nombre == entidad.Nombre).Select(x => x).FirstOrDefault();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            return clienteTemp;
+          //fin
+        }
+    }
+}
---
+++ MantenimientoCliente/MantenimientoClienteDAO/Generico.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data;
+
+using MantenimientoCliente.Models;
+
+namespace MantenimientoClienteDAO
+{
+    public abstract class Generico<T> where T:class
+    {
+        public Generico() { }
+
+        public T Insertar(T entidad)
+        {
+            using (var context = new MantenimientoClienteEntities())
+            {
+                try
+                {
+                    context.Set<T>().Add(entidad);
+                    context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            return entidad;
+        }
+
+        public void Eliminar(T entidad)
+        {
+            using (var context = new MantenimientoClienteEntities())
+            {
+                try
+                {
+                    context.Set<T>().Attach(entidad);
+                    context.Set<T>().Remove(entidad);
+                    context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        public List<T> Listar()
+        {
+            List<T> entidades = null;
+
+            using (var context = new MantenimientoClienteEntities())
+            {
+                try
+                {
+                    entidades = context.Set<T>().Select(x => x).ToList();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            return entidades;
+        }
+
+        public void Actualizar(T entidad)
+        {
+            using (var context = new MantenimientoClienteEntities())
+            {
+                try
+                {
+                    context.Set<T>().Attach(entidad);
+                    context.Entry(entidad).State = EntityState.Modified ;
+                    context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        public abstract T Obtener(int id);
+        public abstract T Buscar(T entidad);
+    }
+}
